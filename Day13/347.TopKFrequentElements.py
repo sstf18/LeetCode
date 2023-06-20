0bb4337fabeadb41e47c3a16969400ac3811eec0
sstf18 <sstf18@gmail.com>
2023-06-20 11:49:20 +0800

    fix day 13

--- Day13/347.TopKFrequentElements.py
+++ Day13/347.TopKFrequentElements.py
@@ -15,6 +15,29 @@ class Solution:
             top_k_freq.append(num)
         return top_k_freq
 
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         # initalize dictinary, store the freq count of each integer in 'nums'
+#         map_ = {}
+#         for i in range(len(nums)):
+#             map_[nums[i]] = map_get(nums[i], 0) + 1
+
+#         # create an empty propority queue
+#         pri_que = [] 
+
+#         # key = i, freq = frequency count
+#         for key, freq in map_.items():
+#             #push a tuple(freq, key) onto the 'pri_que'
+#             heapq.heappush(pri_que, (freq, key))
+#             if len(pri_que) > k: 
+#                 # remove the samllest element from the 'pri_que'
+#                 heapq.headpop(pri_que)
+#         result = [0]*k
+#         for i in range(k-1, -1, -1):
+#             result[i] = heapq.heappop(pri_que)[1]
+
+#         return result
+
 # Thinking: 
 # using map to find the frequency of each elements 
 # += 1 for specific elements, 
