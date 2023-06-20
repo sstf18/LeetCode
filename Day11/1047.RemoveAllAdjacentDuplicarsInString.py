164bd6daf8b3ec761837489ad6b0fabee781b695
sstf18 <sstf18@gmail.com>
2023-06-20 11:31:51 +0800

    add nore way to represent stack in list

--- Day11/1047.RemoveAllAdjacentDuplicarsInString.py
+++ Day11/1047.RemoveAllAdjacentDuplicarsInString.py
@@ -8,6 +8,16 @@ class Solution:
                 res.append(s[i])
         return "".join(res) 
 
+# class Solution:
+#     def removeDuplicates(self, s: str) -> str:
+#         stack = []
+#         for i in range(len(s)):
+#             if stack and s[i] == stack[-1]:
+#                 stack.pop()
+#             else: 
+#                 stack.append(s[i])
+#         return "".join(stack) 
+
 # Thinking: 
 # using the list() as the data strucutres of the stack
 # is res is not empty and s[i] == res[-1](last element)
