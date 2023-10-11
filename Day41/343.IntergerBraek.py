d161e1022f6c7e0b4c27536720087bd5b5bb0e81
sstf18 <sstf18@gmail.com>
2023-10-10 23:18:05 -0400

    dp review

--- Day41/343.IntergerBraek.py
+++ Day41/343.IntergerBraek.py
@@ -6,6 +6,9 @@ class Solution:
         dp[2] = 1
 
         for i in range(3, n + 1):
+            # i//2 + 1: by the math, if the number close to each other, the produce of numbers are biggest. 
             for j in range(1, i // 2 + 1):
+                # j * (i - j): break into two numbers
+                # j * dp[i -j]: dp[i - j] is the break rest of (i-j)
                 dp[i] = max(dp[i], (i - j) * j, dp[i - j] * j)
         return dp[n]
