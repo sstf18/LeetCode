acc32ad28811ce41ab34c3c28b65ca3e15cbdff7
sstf18 <sstf18@gmail.com>
2023-10-13 11:01:49 -0400

    review0-1backpack

--- Day42/0-1_backPack.py
+++ Day42/0-1_backPack.py
@@ -1,11 +1,17 @@
+# dp[i][j]: randomly pick [0-i] from 0 to i items, put into the backpack which weigted by "j"
+# formula: two situation 1. cannot put in bag: dp[i][j] = dp[i - 1]; 2. can put in bag: dp[i][j] = dp[i-1][j]
+#  dp init: 
+#
+
 def test_2_wei_bag_problem1():
     weight = [1, 3, 4]
     value = [15, 20, 30]
     bagweight = 4
 
+    # create a 2d array 
     dp = [[0] * (bagweight + 1) for _ in range(len(weight))]
 
-    # init
+    # init, 
     for j in range(weight[0], bagweight + 1):
         dp[0][j] = value[0]
 
@@ -14,6 +20,7 @@ def test_2_wei_bag_problem1():
         for j in range(bagweight + 1):   # train backpack 
             if j < weight[i]:
                 # cannot put in bag
+                # dp[i - 1] means: the last item before "i"
                 dp[i][j] = dp[i - 1][j]
             else: 
                 # put in bag
--- Day42/0-1_backPack2.py
+++ Day42/0-1_backPack2.py
@@ -1,12 +1,19 @@
+# dp arrary: dp[j] = the biggest value for j
+# formula: max(dp[j], dp[j - weight[i] + value[i]])
+# dp init : dp[0] = 0 
+# train sequence: back to front sequnce
+#
+
 def test_1_wei_bag_problem():
     weight = [1, 3, 4]
     value = [15, 20, 30]
     bagWeight = 4
 
+    # dp init 
     dp = [0] * (bagWeight + 1)
     for i in range(len(weight)):
         for j in range(bagWeight, weight[i] - 1, -1):
-            dp[j] = max(dp[j], dp[j - weight[i] + value[i]])
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i])
     print(dp[bagWeight])
 
 test_1_wei_bag_problem()
